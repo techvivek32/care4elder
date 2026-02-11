aba844f878ea0c453e39acb24ddc7a48a52817f4
Yash Kathiriya <yashkathiriya370@gmail.com>
2026-02-12 02:08:05 +0530

    ecosystem fix

--- admin_dashboard/ecosystem.config.cjs
+++ admin_dashboard/ecosystem.config.cjs
@@ -1,14 +1,13 @@
-module.exports = { 
-  apps: [ 
-    { 
-      name: "care4elder-admin", 
-      cwd: "/var/www/care4elder/admin_dashboard", 
-      script: "node_modules/next/dist/bin/next", 
-      args: "start -p 3000", 
-      env: { 
-        NODE_ENV: "production", 
-        PORT: "3000" 
-      } 
-    } 
-  ] 
-} 
+module.exports = {
+  apps: [
+    {
+      name: "care4elder-admin",
+      cwd: "/var/www/care4elder/admin_dashboard",
+      script: "node_modules/next/dist/bin/next",
+      args: "start -p 3001",
+      env: {
+        NODE_ENV: "production"
+      }
+    }
+  ]
+}
